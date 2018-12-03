c75392c561bdef019f475c149c261c3ba41ff261
Azim <ahmedmohmmadullah@gmail.com>
2018-12-03 18:15:38 +0600

    ci commit

--- app/src/main/java/org/pestrada/android_tdd_playground/MainActivity.java
+++ app/src/main/java/org/pestrada/android_tdd_playground/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);//CI commit
+        setContentView(R.layout.activity_main);
     }
 
     public void changeText(View view) {
