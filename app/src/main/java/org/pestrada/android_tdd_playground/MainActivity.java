e027799d34c456afc28f70861115399a634251a4
Azim <ahmedmohmmadullah@gmail.com>
2018-12-03 13:03:02 +0600

    ci commit

--- app/src/main/java/org/pestrada/android_tdd_playground/MainActivity.java
+++ app/src/main/java/org/pestrada/android_tdd_playground/MainActivity.java
@@ -1,7 +1,7 @@
 package org.pestrada.android_tdd_playground;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);//CI commit
     }
 
     public void changeText(View view) {
